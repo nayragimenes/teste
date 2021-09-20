700b3fe59cb991b9717355ab8759b08bb9b9f84e
Nayra Gimenes Danni <nayragimenes@gmail.com>
2021-09-20 16:29:37 -0300

    Projeto inicial mostrando ola mundo

---
+++ Teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("oi mundo");
+            Console.ReadLine();
+        }
+    }
+}
